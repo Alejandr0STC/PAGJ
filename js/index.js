5317ed851b907ed511bb997ea1af0400f5f353ea
Ajendr0STC <ralejandrob2000@gmail.com>
2023-04-30 23:23:08 -0500

    typed

--- js/index.js
+++ js/index.js
@@ -4,9 +4,16 @@ function insertPetalos(){
     for (let i = 0; i < 48; i++) {
         girasol.innerHTML += '<div class="petalo"> <div class="petalo-curvo"></div> </div>'
     }
-    girasol.innerHTML += `
+    // girasol.innerHTML += `
+    // <div class="centro" style="left: 165.2px;">
+    // <div class="centroText">Tú</div>
+    // </div>
+    // `
+     girasol.innerHTML += `
     <div class="centro" style="left: 165.2px;">
-    <div class="centroText">Tú</div>
+    <div class="centroText">
+        <span class="typed"></span>
+    </div>
     </div>
     `
     AnimacionAire();
@@ -99,9 +106,42 @@ function parrafos(a) {
         
     }
 }
-// Crear una nueva hoja de estilo
+const typed = new Typed('.typed', {
+	strings: [
+		'<i class="verboYou">Tú</i>',
+		'<i class="verboYou">Mi Amor</i>',
+		'<i class="verboYou">Mi Solecito</i>',
+		'<i class="verboYou">Mi todo</i>'
+	],
 
+	//stringsElement: '#cadenas-texto', // ID del elemento que contiene cadenas de texto a mostrar.
+	typeSpeed: 75, // Velocidad en mlisegundos para poner una letra,
+	startDelay: 300, // Tiempo de retraso en iniciar la animacion. Aplica tambien cuando termina y vuelve a iniciar,
+	backSpeed: 75, // Velocidad en milisegundos para borrrar una letra,
+	smartBackspace: true, // Eliminar solamente las palabras que sean nuevas en una cadena de texto.
+	shuffle: false, // Alterar el orden en el que escribe las palabras.
+	backDelay: 1500, // Tiempo de espera despues de que termina de escribir una palabra.
+	loop: true, // Repetir el array de strings
+	loopCount: false, // Cantidad de veces a repetir el array.  false = infinite
+	showCursor: true, // Mostrar cursor palpitanto
+	cursorChar: '|', // Caracter para el cursor
+	contentType: 'html', // 'html' o 'null' para texto sin formato
+});
+// const titulo = new Typed('.typedt', {
+// 	strings: [
+// 		'Para mi solecito, un girasol del que tanto le gusta'
+// 	],
 
-// Agregar una regla de animación a la hoja de estilo
-
-
+// 	//stringsElement: '#cadenas-texto', // ID del elemento que contiene cadenas de texto a mostrar.
+// 	typeSpeed: 75, // Velocidad en mlisegundos para poner una letra,
+// 	startDelay: 300, // Tiempo de retraso en iniciar la animacion. Aplica tambien cuando termina y vuelve a iniciar,
+// 	backSpeed: 75, // Velocidad en milisegundos para borrrar una letra,
+// 	smartBackspace: true, // Eliminar solamente las palabras que sean nuevas en una cadena de texto.
+// 	shuffle: false, // Alterar el orden en el que escribe las palabras.
+// 	backDelay: 1500, // Tiempo de espera despues de que termina de escribir una palabra.
+// 	loop: false, // Repetir el array de strings
+// 	loopCount: true, // Cantidad de veces a repetir el array.  false = infinite
+// 	showCursor: true, // Mostrar cursor palpitanto
+// 	cursorChar: '|', // Caracter para el cursor
+// 	contentType: 'html', // 'html' o 'null' para texto sin formato
+// });
