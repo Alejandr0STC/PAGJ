9ef6d8a7961a113e9a9850c32fed6adea42a6900
Ajendr0STC <ralejandrob2000@gmail.com>
2023-05-09 22:39:15 -0500

    music

--- js/index.js
+++ js/index.js
@@ -127,21 +127,7 @@ const typed = new Typed('.typed', {
 	cursorChar: '|', // Caracter para el cursor
 	contentType: 'html', // 'html' o 'null' para texto sin formato
 });
-// const titulo = new Typed('.typedt', {
-// 	strings: [
-// 		'Para mi solecito, un girasol del que tanto le gusta'
-// 	],
-
-// 	//stringsElement: '#cadenas-texto', // ID del elemento que contiene cadenas de texto a mostrar.
-// 	typeSpeed: 75, // Velocidad en mlisegundos para poner una letra,
-// 	startDelay: 300, // Tiempo de retraso en iniciar la animacion. Aplica tambien cuando termina y vuelve a iniciar,
-// 	backSpeed: 75, // Velocidad en milisegundos para borrrar una letra,
-// 	smartBackspace: true, // Eliminar solamente las palabras que sean nuevas en una cadena de texto.
-// 	shuffle: false, // Alterar el orden en el que escribe las palabras.
-// 	backDelay: 1500, // Tiempo de espera despues de que termina de escribir una palabra.
-// 	loop: false, // Repetir el array de strings
-// 	loopCount: true, // Cantidad de veces a repetir el array.  false = infinite
-// 	showCursor: true, // Mostrar cursor palpitanto
-// 	cursorChar: '|', // Caracter para el cursor
-// 	contentType: 'html', // 'html' o 'null' para texto sin formato
-// });
+window.addEventListener('load', function() {
+    document.getElementById("music1").volume = 0.5
+    document.getElementById("music1").play()
+});
